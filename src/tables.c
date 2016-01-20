8b49a76b0fd91803ce00b4ffeb261d776009a61e
Peter Lee <plee831@berkeley.edu>
2016-01-19 21:06:26 -0800

    initial commit

---
+++ src/tables.c
@@ -0,0 +1,155 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "utils.h"
+#include "tables.h"
+
+const int SYMTBL_NON_UNIQUE = 0;
+const int SYMTBL_UNIQUE_NAME = 1;
+
+/*******************************
+ * Helper Functions
+ *******************************/
+
+void allocation_failed() {
+    write_to_log("Error: allocation failed\n");
+    exit(1);
+}
+
+void addr_alignment_incorrect() {
+    write_to_log("Error: address is not a multiple of 4.\n");
+}
+
+void name_already_exists(const char* name) {
+    write_to_log("Error: name '%s' already exists in table.\n", name);
+}
+
+void write_symbol(FILE* output, uint32_t addr, const char* name) {
+    fprintf(output, "%u\t%s\n", addr, name);
+}
+
+/*******************************
+ * Symbol Table Functions
+ *******************************/
+
+/* Creates a new SymbolTable containg 0 elements and returns a pointer to that
+   table. Multiple SymbolTables may exist at the same time. 
+   If memory allocation fails, you should call allocation_failed(). 
+   Mode will be either SYMTBL_NON_UNIQUE or SYMTBL_UNIQUE_NAME. You will need
+   to store this value for use during add_to_table().
+ */
+SymbolTable* create_table(int mode) {
+    SymbolTable* symboltable = (SymbolTable*) malloc(sizeof(SymbolTable));
+    if (symboltable == NULL) {
+	allocation_failed();
+    }
+    symboltable->cap = 10;
+    symboltable->tbl = NULL;
+    symboltable->tbl = (Symbol*) malloc (symboltable->cap * sizeof(Symbol));
+    if (symboltable->tbl == NULL) {
+	free(symboltable);
+	allocation_failed();
+    }
+    symboltable->len = 0;
+    symboltable->mode = mode;
+    return symboltable;
+}
+
+/* Frees the given SymbolTable and all associated memory. */
+void free_table(SymbolTable* table) {
+    for (int i = 0; i < table->len; i++) {
+	Symbol* pointer = table->tbl+i;
+	if (pointer->name != NULL) {
+	    free(pointer->name);
+	}
+    }
+    if (table->tbl != NULL) {
+	free(table->tbl);
+    }
+    if (table != NULL) {
+	free(table);
+    }
+}
+
+/* Adds a new symbol and its address to the SymbolTable pointed to by TABLE. 
+   ADDR is given as the byte offset from the first instruction. The SymbolTable
+   must be able to resize itself as more elements are added. 
+
+   Note that NAME may point to a temporary array, so it is not safe to simply
+   store the NAME pointer. You must store a copy of the given string.
+
+   If ADDR is not word-aligned, you should call addr_alignment_incorrect() and
+   return -1. If the table's mode is SYMTBL_UNIQUE_NAME and NAME already exists 
+   in the table, you should call name_already_exists() and return -1. If memory
+   allocation fails, you should call allocation_failed(). 
+
+   Otherwise, you should store the symbol name and address and return 0.
+ */
+int add_to_table(SymbolTable* table, const char* name, uint32_t addr) {
+    if (addr%4 != 0) {
+	addr_alignment_incorrect();
+	return -1;
+    }
+    if (table->mode == SYMTBL_UNIQUE_NAME) {
+	Symbol* copy_table = table->tbl;
+	int j = 0;
+	while (j < table->len && copy_table != NULL) { 
+	    if (strcmp(copy_table->name, name) == 0) {
+		name_already_exists(name);
+		return -1;
+	    }
+	    copy_table++;
+	    j++;
+	}
+    }
+    if (table->cap == table->len) {
+	table->cap = table->cap * 2;
+	Symbol* temp_table = (Symbol*)realloc (table->tbl, table->cap * sizeof(Symbol));
+	if (temp_table == NULL) {
+	    allocation_failed();
+	}
+	table->tbl = temp_table;
+    }
+    int i = table->len;
+    int j = 0;
+    Symbol* cpy_table = table->tbl;
+    while (j < i) {
+	cpy_table++;
+	j++;
+    }
+    cpy_table->name = (char*) malloc((strlen(name) + 1) *sizeof(char));
+    strcpy(cpy_table->name, name);
+    cpy_table->addr = addr;
+    
+    table->len++;
+    return 0;
+}
+
+/* Returns the address (byte offset) of the given symbol. If a symbol with name
+   NAME is not present in TABLE, return -1.
+ */
+int64_t get_addr_for_symbol(SymbolTable* table, const char* name) {
+    Symbol* table_pointer = table->tbl;
+    int i = 0;
+    while (i < table->len && table_pointer != NULL) {
+	if (strcmp(table_pointer->name, name) == 0) {
+	    return table_pointer->addr;
+	}
+	table_pointer++;
+	i++;
+    }
+    return -1;
+}
+
+/* Writes the SymbolTable TABLE to OUTPUT. You should use write_symbol() to
+   perform the write. Do not print any additional whitespace or characters.
+ */
+void write_table(SymbolTable* table, FILE* output) {
+    Symbol* table_pointer = table->tbl;
+    while (table_pointer != NULL) {
+    write_symbol(output, table_pointer->addr, table_pointer->name);
+    	table_pointer++;
+    }
+}
